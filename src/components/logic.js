8d7d85a90cc3bdf4afe707cbef48f0bd885b6a82
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-18 16:11:28 +0300

    Update handler and start render

--- src/components/logic.js
+++ src/components/logic.js
@@ -1,8 +1,6 @@
 
 export const getWeatherData = async (location) => {
     const API_KEY = import.meta.env.VITE_API_KEY;
-    console.log(location);
-
     const unitGroup = "metric";
     const contentType = "json";
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=${API_KEY}&contentType=${contentType}`;
@@ -15,6 +13,7 @@ export const getWeatherData = async (location) => {
         }
         const data = await response.json();
         console.log("Weather data:", data);
+        return data;
     } catch (err) {
         console.error("Failed to fetch data", err);
     }
--- src/components/ui.js
+++ src/components/ui.js
@@ -1,14 +1,37 @@
-export function userRequest(handleLocation) {
+
+
+export function userRequest(handler) {
 
     const form = document.querySelector(".userForm");
     const input = document.querySelector("#locationInput");
 
 
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
 
-        const location = input.value.trim();
+        const userInput = input.value.trim(); // sends userInput
 
-        handleLocation(location)
+        const data = await handler(userInput); //returns data
+        renderWeather(data)
     })
+}
+
+
+export function renderWeather(data) {
+    const body = document.querySelector("#app");
+    const renderContainer = document.createElement("div");
+    const address = document.createElement("p");
+    address.textContent = data.address;
+    const conditions = document.createElement("p");
+    
+    const sunrise = document.createElement("p");
+    const sunset = document.createElement("p");
+
+    const temp = document.createElement("p");
+    
+
+    body.appendChild(renderContainer);
+
+    renderContainer.append(address, conditions, sunrise, sunset, temp);
+
 }
